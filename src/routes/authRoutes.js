9a1de984c8def22959f17aecccc1bc80d5793913
kmarks2013 <kmarks2013@gmail.com>
2020-07-23 14:55:19 -0400

    adds basic error handling

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -1,10 +1,23 @@
 const express = require('express')
+const mongoose = require('mongoose')
+const User = mongoose.model('User')
 
 const router = express.Router()
 
-router.post('/signup', (req, res) => {
-    console.log(req.body)
-    res.send("you made a post request")
+router.post('/signup', async (req, res) => {
+    //console.log(req.body)
+    const {email, password} = req.body
+
+    try{
+        const user = new User({email, password})
+        await user.save()
+        res.send("you made a post request")
+
+    }  catch (err){
+        return res.status(422).send(err.message)
+        // res.send(err)
+    }
+
 })
 
 module.exports = router
